9119f526a34660ac338cd1840f64376d09bccf5d
Bradford Wong <bmwwong96@gmail.com>
2019-04-11 09:19:47 -0700

    Added beginning of replies part. It works for the most part, but need to incorporate it with the database.

--- app/src/main/java/com/myvetpath/myvetpath/SubDetailsActivity.java
+++ app/src/main/java/com/myvetpath/myvetpath/SubDetailsActivity.java
@@ -1,14 +1,18 @@
 package com.myvetpath.myvetpath;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.FileInputStream;
 import java.text.SimpleDateFormat;
@@ -18,8 +22,32 @@ import java.util.Date;
 
 import static java.sql.Types.NULL;
 
-public class SubDetailsActivity extends BaseActivity {
+public class SubDetailsActivity extends BaseActivity implements AddReplyCustomDialog.OnInputListener {
 
+    @Override
+    public void sendInput(String input) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(SubDetailsActivity.this);
+        String userName = preferences.getString(getString(R.string.username_preference_key), "");
+
+        if(userName.equals("")){//if user isn't logged in, then make them login first
+            Toast.makeText(SubDetailsActivity.this, "Please login first " + userName,
+                    Toast.LENGTH_LONG).show();
+            Intent login_activity;
+            login_activity = new Intent(SubDetailsActivity.this, LoginActivity.class);
+            startActivity(login_activity);
+            return;
+        }
+
+
+        //Get current date, might want to change date to when it gets to server
+        long curDate = Calendar.getInstance().getTime().getTime();
+        calendar.setTimeInMillis(curDate);
+        String currentDate = simpleDateFormat.format(calendar.getTime());
+
+        //Set text and add to database
+        mReplies += "(" + currentDate + ")" + input + "\n \n";
+        mRepliesTV.setText(mReplies);
+    }
 
     Intent create_sub_activity;
     MyDBHandler myDBHandler;
@@ -29,6 +57,11 @@ public class SubDetailsActivity extends BaseActivity {
     final SimpleDateFormat simpleDateFormat = new SimpleDateFormat();
     private TextView mSamplesTV;
     private ImageButton[] images;
+    private Button add_replies_BTTN;
+    private TextView mRepliesTV;
+    private String mReplyInput;
+    private String mReplies;
+    private ArrayList<Reply> mRepliesList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,7 +94,7 @@ public class SubDetailsActivity extends BaseActivity {
             String tempSampleDate = simpleDateFormat.format(calendar.getTime());
 
             sampleText += "Sample " + tempSample.getNameOfSample() + ": " + tempSample.getNumberOfSamples() + " samples collected "
-                    + " in " + tempSample.getLocation() + " on " + tempSampleDate + "\n";
+                    + " in " + tempSample.getLocation() + " on " + tempSampleDate + "\n\n";
             index++;
         }
 
@@ -104,6 +137,32 @@ public class SubDetailsActivity extends BaseActivity {
         TextView commentText = findViewById(R.id.subComment);
         commentText.setText(comment);
 
+        //Set up Replies
+        mRepliesTV = findViewById(R.id.subRepliesTV);
+        add_replies_BTTN = findViewById(R.id.add_reply_BTTN);
+        mReplies = "";
+        mRepliesList = new ArrayList<>();// set this equal to what we have in database
+
+        Reply tempReply = new Reply();
+        tempReply.setMessage("Here's my first message");
+        mRepliesList.add(tempReply);
+
+        for(Reply reply : mRepliesList){
+            mReplies += reply.getMessage() + "\n";
+        }
+
+        mRepliesTV.setText(mReplies);
+
+        add_replies_BTTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AddReplyCustomDialog dialog = new AddReplyCustomDialog();
+                dialog.show(getFragmentManager(), "AddReplyCustomDialog");
+
+
+            }
+        });
+
 
         //set images
         images = new ImageButton[]{findViewById(R.id.first_ImageDetails_bttn), findViewById(R.id.second_ImageDetails_bttn),
@@ -132,6 +191,7 @@ public class SubDetailsActivity extends BaseActivity {
 
 
     }
+
 
 
 }
